7fa1af1a93f8ffa89e1a48584f1c12715059660f
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2018-11-24 16:55:38 +0000

    add welcome controller

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,8 @@
+<head>
+  <title>@yield('title','LaraPictGram')</title>
+</head>
+<h1>Welcome</h1>
+
+<body>
+@yield('content')
+</body>
--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.master')
+
 @section('content')
     <p>
         Hello! And welcome to pictogram. The ultimate combo of pictionary and instagram.
         Draw, compare, see who can guess you picture first.
     </p>
-
-@stop
+@endsection
